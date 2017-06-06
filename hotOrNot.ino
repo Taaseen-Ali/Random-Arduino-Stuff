4b2f219afab35182c305a82193b763ee8cb3c2af
Taaseen Ali <taaseen.ali89@gmail.com>
2017-06-05 23:51:52 -0400

    Add files via upload

---
+++ hotOrNot.ino
@@ -0,0 +1,158 @@
+/*
+  Arduino Starter Kit example
+ Project 3  - Love-O-Meter
+
+ This sketch is written to accompany Project 3 in the
+ Arduino Starter Kit
+
+ Parts required:
+ 1 TMP36 temperature sensor
+ 3 red LEDs
+ 3 220 ohm resistors
+
+ Created 13 September 2012
+ by Scott Fitzgerald
+
+ http://www.arduino.cc/starterKit
+
+ This example code is part of the public domain
+ */
+
+// named constant for the pin the sensor is connected to
+#include <Servo.h>
+Servo myServo;
+
+const int sensorPin = A0;
+const int potPin = A1;
+const int ledPin = 4;
+const int buttonPin = 7;
+int buttonState = LOW;
+float baselineTemp = 0;
+// room temperature in Celcius
+int potVal = 0;
+float angle;
+float sensorValue = 0;         // the sensor value
+float sensorMin = 1023;        // minimum sensor value
+float sensorMax = 0;           // maximum sensor value
+float sensorAvg;
+float baselineVoltage;
+float upperBound = 0;
+
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  digitalWrite(ledPin, HIGH);
+  while (millis() < 5000) {
+    sensorValue = analogRead(sensorPin);
+
+    // record the maximum sensor value
+    if (sensorValue > sensorMax) {
+      sensorMax = sensorValue;
+    }
+
+    // record the minimum sensor value
+    if (sensorValue < sensorMin) {
+      sensorMin = sensorValue;
+    }
+  }
+  // open a serial connection to display values
+  myServo.attach(3);
+  Serial.begin(9600);
+  
+  Serial.println(sensorMin);
+  Serial.println(sensorMax);
+  sensorAvg = (sensorMax + sensorMin)/2;
+  Serial.println(sensorAvg);
+  
+  float baselineVoltage = (sensorMin / 1024.0) * 5.0;
+  Serial.println(baselineVoltage);
+  baselineTemp = (baselineVoltage - .5) * 100;
+  Serial.println(baselineTemp);
+  digitalWrite(ledPin, LOW);
+  
+}
+
+
+void calibrate() {
+  digitalWrite(ledPin, HIGH);
+  while (millis() < 5000) {
+    sensorValue = analogRead(sensorPin);
+    
+    // record the maximum sensor value
+    if (sensorValue > sensorMax) {
+      sensorMax = sensorValue;
+      }
+    
+    // record the minimum sensor value
+    if (sensorValue < sensorMin) {
+      sensorMin = sensorValue;
+      }
+    }
+    delay(5000);
+    Serial.println(sensorMin);
+    Serial.println(sensorMax);
+    sensorAvg = (sensorMax + sensorMin)/2;
+    Serial.println(sensorAvg);
+      
+  //  float baselineVoltage = (sensorAvg / 1024.0) * 5.0;
+    float baselineVoltage = (sensorMin/1024)*5.0;
+    Serial.println(baselineVoltage);
+    baselineTemp = (baselineVoltage - .5) * 100 - 1.5;
+    Serial.println(baselineTemp);
+    digitalWrite(ledPin, LOW);
+}
+
+
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+  
+  if(buttonState == HIGH){
+      calibrate();  
+    }
+
+  Serial.print("potVal,upperBound:  ");
+  Serial.print(upperBound);
+  
+  Serial.print(" baselineTemp: ");
+  Serial.print(baselineTemp);
+  // read the value on AnalogIn pin 0
+  // and store it in a variable
+  potVal = analogRead(potPin);
+  int sensorVal = analogRead(sensorPin);
+
+  // send the 10-bit sensor value out the serial port
+  Serial.print(", sensor Value: ");
+  Serial.print(sensorVal);
+
+  // convert the ADC reading to voltage
+  float voltage = (sensorVal / 1024.0) * 5.0;
+
+  // Send the voltage level out the Serial port
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+
+  // convert the voltage to temperature in degrees C
+  // the sensor changes 10 mV per degree
+  // the datasheet says there's a 500 mV offset
+  // ((volatge - 500mV) times 100)
+  Serial.print(", degrees C: ");
+  float temperature = (voltage - .5) * 100;
+  Serial.print(temperature);
+
+  // if the current temperature is lower than the baseline
+  // turn off all LEDs
+  upperBound = map(potVal, 0, 1023, 0.0 ,8.0);
+  if(upperBound == 0){
+    digitalWrite(ledPin, HIGH);
+    delay(1000);
+    digitalWrite(ledPin, LOW);
+  }
+  angle = map(temperature,  baselineTemp, baselineTemp + upperBound, 0, 179);
+  Serial.print(", angle: ");
+  Serial.println(angle);
+  myServo.write(angle);
+  delay(1000);
+}
